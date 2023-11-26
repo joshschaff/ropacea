37026918180ddd2096d51d8b268fcc29189f8ff0
joshschaff <joshuaischaffer@gmail.com>
2023-11-26 16:40:34 -0600

    bug fixes

--- ropacea/backtest.py
+++ ropacea/backtest.py
@@ -52,5 +52,5 @@ if __name__ == '__main__':
     out = backtest_loop(
         start_date = date(year = 2017, month=1, day=1),
         end_date = date(2023, month=1, day=1),
-        strategy=PortfolioStrategy.EQUALLY_WEIGHTED
+        strategy=PortfolioStrategy.SINGLE_FACTOR
     )
