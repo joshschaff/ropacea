d5c5d16b790e33f107265906f3e7a722cd2d602b
iruble23 <142930897+iruble23@users.noreply.github.com>
2023-11-26 16:53:04 -0600

    changes

--- ropacea/scs.py
+++ ropacea/scs.py
@@ -5,11 +5,9 @@ import numpy as np
 import pandas as pd
 
 def scs(data):
-    mark_date = date(year = 2017, month=1, day=1)
-    sample_months = 60
-    data_copy = data.get_in_sample_data(mark_date, sample_months)
+
     # Pivot the data to create a DataFrame suitable for covariance calculation
-    pivot_data = data_copy.pivot_table(values='Monthly Total Return', 
+    pivot_data = data.pivot_table(values='Monthly Total Return', 
                                     index='Monthly Calendar Date', 
                                     columns='Ticker')
 
@@ -29,7 +27,7 @@ def scs(data):
     # Estimate the optimal shrinkage coefficient (Ledoit-Wolf method)
     delta = np.mean(np.diag(sample_cov_matrix - target_cov_matrix))
     gamma = np.linalg.norm(sample_cov_matrix - target_cov_matrix, ord='fro') ** 2
-    n_obs = data_copy['Monthly Calendar Date'].nunique()  # Number of unique dates (observations)
+    n_obs = data['Monthly Calendar Date'].nunique()  # Number of unique dates (observations)
 
     alpha = max(0, (gamma - n_assets) / (delta * n_obs))
 
