c80aa344c77fb762f87e9b6500c825161c360f4b
kjarjoura <ktj2@rice.edu>
2023-11-26 16:39:17 -0600

    made it function

--- ropacea/const_corr.py
+++ ropacea/const_corr.py
@@ -1,41 +1,49 @@
-from datetime import date
-import data
-import numpy as np
-import pandas as pd
+def constant_corr(import_data):
 
-mark_date = date(year = 2017, month=1, day=1)
-sample_months = 60
+    from datetime import date
+    import data
+    import numpy as np
+    import pandas as pd
 
-data_copy = data.get_in_sample_data(mark_date, sample_months)
+    mark_date = date(year = 2017, month=1, day=1)
+    sample_months = 60
 
-print(data_copy)
+    data_copy = import_data.get_in_sample_data(mark_date, sample_months)
+
+    # print(data_copy)
 
 
-# Estimation of Individual Asset Volatilities
-sigma = data_copy.groupby('Ticker')['Monthly Total Return'].std()
-print("sigma")
-print(sigma)
+    # Estimation of Individual Asset Volatilities
+    sigma = data_copy.groupby('Ticker')['Monthly Total Return'].std()
+    # print("sigma")
+    # print(sigma)
 
-# Estimation of Pairwise Correlations
+    # Estimation of Pairwise Correlations
 
-# Pivot the DataFrame to get 'Monthly Total Return' for each 'Ticker' in columns
-pivot_data = data_copy.pivot(index='Monthly Calendar Date', columns='Ticker', values='Monthly Total Return')
+    # Pivot the DataFrame to get 'Monthly Total Return' for each 'Ticker' in columns
+    pivot_data = data_copy.pivot(index='Monthly Calendar Date', columns='Ticker', values='Monthly Total Return')
 
-# Calculate the correlation matrix
-rho = pivot_data.corr()
-print("rho")
-print(rho)
+    # Calculate the correlation matrix
+    rho = pivot_data.corr()
+    # print("rho")
+    # print(rho)
+    avg_corr = rho.values[np.triu_indices(len(rho), k=1)].mean()
+    # print("Average Correlation:", avg_corr)
 
- # Construct Covariance Matrix using Constant Correlation Model
-V = rho.values * np.dot(sigma.values, sigma.values.T) + (1 - rho.values) * np.diag(sigma.values)**2
-V_df = pd.DataFrame(V, index=rho.index, columns=rho.columns)
 
-print("Constant Correlation Covariance Matrix:")
-print(V_df)
+    # Construct Covariance Matrix using Constant Correlation Model
+    V = avg_corr * np.dot(sigma.values, sigma.values.T) + (1 - avg_corr) * np.diag(sigma.values)**2
+    #V = rho.values * np.dot(sigma.values, sigma.values.T) + (1 - rho.values) * np.diag(sigma.values)**2
+    V_df = pd.DataFrame(V, index=rho.index, columns=rho.columns)
 
-import openpyxl
+    # print("Constant Correlation Covariance Matrix:")
+    # print(V_df)
 
-excel_file_path = '/Users/kathrynjarjoura/Desktop/V_cc.xlsx'
+    return V_df
 
-# Export the DataFrame to Excel
-V_df.to_excel(excel_file_path, index=True)
+    # import openpyxl
+
+    # excel_file_path = '/Users/kathrynjarjoura/Desktop/V_cc17.xlsx'
+
+    # # Export the DataFrame to Excel
+    # V_df.to_excel(excel_file_path, index=True)
